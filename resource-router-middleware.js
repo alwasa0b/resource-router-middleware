86f01e053dbf2b505f7306957863808b420482b5
Saeed Alalwan <alalwan@gmail.com>
2018-06-21 16:13:01 -0400

    Add custom put routes.

--- resource-router-middleware.js
+++ resource-router-middleware.js
@@ -1,34 +1,54 @@
-var Router = require('express').Router;
+var Router = require("express").Router;
 
-var keyed = ['get', 'read', 'put', 'update', 'patch', 'modify', 'del', 'delete'],
-	map = { index:'get', list:'get', read:'get', create:'post', update:'put', modify:'patch' };
+var keyed = [
+    "get",
+    "read",
+    "put",
+    "update",
+    "patch",
+    "modify",
+    "del",
+    "delete"
+  ],
+  map = {
+    index: "get",
+    list: "get",
+    read: "get",
+    create: "post",
+    update: "put",
+    modify: "patch"
+  };
 
 module.exports = function ResourceRouter(route) {
-	route.mergeParams = route.mergeParams ? true : false;
-	var router = Router({mergeParams: route.mergeParams}),
-		key, fn, url;
+  route.mergeParams = route.mergeParams ? true : false;
+  var router = Router({ mergeParams: route.mergeParams }),
+    key,
+    fn,
+    url;
 
-	if (route.middleware) router.use(route.middleware);
+  if (route.middleware) router.use(route.middleware);
 
-	if (route.load) {
-		router.param(route.id, function(req, res, next, id) {
-			route.load(req, id, function(err, data) {
-				if (err) return res.status(404).send(err);
-				req[route.id] = data;
-				next();
-			});
-		});
-	}
+  if (route.load) {
+    router.param(route.id, function(req, res, next, id) {
+      route.load(req, id, function(err, data) {
+        if (err) return res.status(404).send(err);
+        req[route.id] = data;
+        next();
+      });
+    });
+  }
 
-	for (key in route) {
-		fn = map[key] || key;
-		if (typeof router[fn]==='function') {
-			url = ~keyed.indexOf(key) ? ('/:'+route.id) : '/';
-			router[fn](url, route[key]);
-		}
-	}
+  for (key in route) {
+    fn = map[key] || key;
+    if (typeof router[fn] === "function") {
+      url = ~keyed.indexOf(key) ? "/:" + route.id : "/";
+      router[fn](url, route[key]);
+    } else if (typeof route[key] === "function") {
+      router.put(`/${key}/:${route.id}`, route[key]);
+    }
+  }
 
-	return router;
+  return router;
 };
 
 module.exports.keyed = keyed;
